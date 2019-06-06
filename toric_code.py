676110f6175c2e0542bcd576ab0a0be7e9792e0a
Sean <38267645+seandiscovery@users.noreply.github.com>
2019-06-05 20:58:59 -0700

    Basic updates to toric code

--- toric_code.py
+++ toric_code.py
@@ -1,7 +1,6 @@
 """
 An implementation of A. Kitaev's toric code (https://arxiv.org/pdf/quant-ph/9707021.pdf). 
 Final project for CS269Q at Stanford University.
-
 Authors: Richard Mu, Sean Mullane, and Chris Yeh 
 (c) 2019 
 """
@@ -14,21 +13,30 @@ def construct_toric_code(L: int) -> List[nx.Graph]:
 	:param L: Number of physical qubits on one side of the square lattice 
 	:returns: Primal graph and dual graph in a List 
 	"""
-	# First step is to generate an L x L grid, where L is the number of physical qubits 
+	# Step 1: generate an L x L grid, where L is the number of physical qubits 
 	# on one side of the grid 
-	# NOTE that the physical qubits are situated at the EDGES, not vertices 
-	# The graph must have periodic boundary conditions
 	
-	# Using the NetworkX package here; allows us to generate an L x L lattice 
-	# with periodic boundary conditions. 
-	# See https://networkx.github.io/documentation/stable/reference/generated/networkx.generators.lattice.grid_2d_graph.html
+	# Using the NetworkX package; allows us to generate an L x L lattice 
+	# with periodic boundary conditions. See:
+	# https://networkx.github.io/documentation/stable/reference/generated/networkx.generators.lattice.grid_2d_graph.html
 	primal_graph = nx.generators.lattice.grid_2d_graph(L, L, periodic=True) 
-	return primal_graph
+	dual_graph = nx.generators.lattice.grid_2d_graph(L, L, periodic=True)
+	
+	# Step 2: Add physical qubits and stabilizer generators to lattice 
+	# Add qubits to edges  
 
-def toric_error_correction(): 
+	# Construct Z-stabilizer generators 
+
+	# Construct X-stabilizer generators   
+
+	return [primal_graph, dual_graph] 
+
+def toric_error_correction() -> List[nx.Graph]: 
 	""" Given a toric code, applies an error model and attempts to correct errors. 
 	"""
 	pass 
+
+
 
 
 
